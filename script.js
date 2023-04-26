1f5171b332046397c9d27500d39255739b3485ea
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-26 10:49:10 -0300

    Pegando Event do Enter e Input para add Tags

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const ul = document.querySelector("ul")
 const input = document.querySelector("input")
 
+const itemList = []
+
+input.addEventListener("keydown", (e) => {
+    if (e.code.includes("Enter") && e.target.value) {
+        itemList.push(e.target.value);
+        input.value = "";
+
+        loadItens()
+    }
+})
